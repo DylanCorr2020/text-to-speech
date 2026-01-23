7c4b4a13d671903a1b83c0990cf88058a71e130f
Dylan Corr <dylancorr.g@gmail.com>
2026-01-23 06:56:53 +0000

    Remove unused useRef import in AudioList

--- frontend/src/components/AudioList.jsx
+++ frontend/src/components/AudioList.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect} from "react";
 import { listAudioFiles } from "../api/listAudioFiles";
 import { deleteAudioFile } from "../api/deleteAudioFiles";
 import { getCurrentUser } from "aws-amplify/auth";
